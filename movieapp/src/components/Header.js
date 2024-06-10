ebbc5bb869375d3e46aa6e42d008d7b1f587f743
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-10 09:59:22 +0530

    setup login and signup page

--- movieapp/src/components/Header.js
+++ movieapp/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { IoIosArrowDropdown } from "react-icons/io";
 
-function Header() {
+const Header = () => {
     return (
-        <div>Header</div>
+        <div className='absolute z-10 flex w-[100vw] items-center justify-between px-6 bg-gradient-to-b from-black'>
+            <img className='w-56' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png' alt='Logo'></img>
+            <div className='flex items-center'>
+                <IoIosArrowDropdown size="24px" color='white' />
+                <h1 className='text-1g font-medium text-white'> Mern Stack   </h1>
+                <div className='ml-4'>
+                    <button className='bg-red-800 text-white px-4 py-2'>Logout</button>
+                    <button className='bg-red-800 text-white px-4 py-2 ml-2'>Watch Movie</button>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
--- movieapp/src/components/Login.js
+++ movieapp/src/components/Login.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 function Login() {
+    const [isLogin, setIsLogin] = useState(false);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const loginHandler = () => {
+        setIsLogin(!isLogin)
+    }
+
+    const getInputData = (e) => {
+        e.preventDefault();    //page will not get reloaded on clicking signup or login button using this
+        console.log(fullName, email, password)
+        setFullName("")
+        setEmail("")
+        setPassword("")
+    }
     return (
         <div>
             <Header />
-            <div>
-                Login
+            <div className='absolute'>
+                <img className='w-[100vw] h-[100vh] bg-cover' src='https://wallpapers.com/images/high/netflix-background-gs7hjuwvv2g0e9fj.webp' alt='background'></img>
             </div>
+            <form onSubmit={getInputData} className='flex flex-col w-3/12 p-12 my-36 left-0 right-0 mx-auto items-center justify-center absolute bg-black opacity-80 rounded-md'>
+                <h1 className='text-3xl text-white mb-5 font-bold'> {isLogin ? "Login" : "Signup"}</h1>
+                <div className='flex flex-col'>
+                    {
+                        !isLogin && <input value={fullName} onChange={(e) => setFullName(e.target.value)} type='text' placeholder='Full Name' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'></input>
+                    }
+
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'></input>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Password' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'></input>
+                    <button className='bg-red-600 mt-6 p-3 text-white rounded-sm font-medium'>{isLogin ? "Login" : "Signup"}</button>
+                    <p className='text-white mt-2'>{isLogin ? "New to Moviezz?" : "Already have an account?"} <span onClick={loginHandler} className='ml-1 text-blue-900 cursor-pointer font-medium'>{isLogin ? "Signup" : "Login"}</span></p>
+                </div>
+            </form>
         </div>
     )
 }
