ffd47957007920b0484d5858ab3737f6884dda2b
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-06-15 14:05:46 +0530

    design video page

--- movieapp/src/components/VideoBackground.js
+++ movieapp/src/components/VideoBackground.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function VideoBackground() {
     return (
-        <div>VideoBackground</div>
+        <div className=' w-screen'>
+            <iframe
+                className='w-screen aspect-video'
+                src="https://www.youtube.com/embed/k3g_WjLCsXM?si=7uqapefTOSD_atzI&autoplay=1&mute=1"
+                title="YouTube video player"
+                frameborder="0"
+                allowfullscreen>
+
+            </iframe>
+        </div>
     )
 }
 
--- movieapp/src/components/VideoTitle.js
+++ movieapp/src/components/VideoTitle.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { CiPlay1 } from "react-icons/ci";
+import { CiCircleInfo } from "react-icons/ci";
+
 
 function VideoTitle() {
     return (
-        <div>
-            <h1 className='text-3xl '></h1>
+        <div className='w-screen aspect-video absolute text-white pt-[18%] p-12'>
+            <h1 className='text-3xl font-bold'>Vaibhav Mern</h1>
+            <p>Lorem ipsum is not simply random text.</p>
+            <div className='flex mt-8'>
+                <button className=' flex items-center px-6 py-2 bg-white text-black rounded-md hover:bg-opacity-80'>
+                    <CiPlay1 size="24px" />
+                    <span className='ml-1'>Play</span>
+                </button>
+                <button className=' flex items-center mx-2 px-6 py-2 bg-gray-500 text-black rounded-md bg-opacity-50'>
+                    <CiCircleInfo size="24px" />
+                    <span className='ml-1'>Watch more</span>
+                </button>
+            </div>
         </div>
     )
 }
